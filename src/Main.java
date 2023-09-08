8d2c82289cfe5042f385f70836d7da20187618f9
CSC1060-Marsh <143460025+CSC1060-Marsh@users.noreply.github.com>
2023-09-08 17:35:29 -0600

    Added Explanations

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,48 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+            //This adds a new scanner system.
         Scanner sc = new Scanner(System.in);
+
+            //This asks the first question, "What's your name?" to the user.
         System.out.println("Hello! What's your name?");
+
+            //This defines the user's next input as the string "name".
         String name = sc.nextLine();
+
+            //This prints out the line asking where the user is from.
         System.out.println("Hello " + name + ", where are you from?");
+
+            //This defines the user's input to the previous question as the string "location".
         String location = sc.nextLine();
+
+            //This asks the user their age.
         System.out.println("How old are you, " + name + "?");
+
+            //This defines the user's answer to the above question as the string "age".
         String age = sc.nextLine();
-        System.out.println("Good morning, " + name + ", you are from " + location + " and are " + age + " years old! Nice to meet you!");
+
+            //This sends a message to the user including their inputs for name, location, and age.
+        System.out.println("Good morning, " + name + ", you are from " + location + " and are "
+                + age + " years old! Nice to meet you!");
+
+            //This asks the user about their favorite activity.
         System.out.println("What is your favorite activity?");
+
+            //This defines the previous response as the string "activity".
         String activity = sc.nextLine();
+
+            //This tells the user to have fun the next time they do the above activity.
         System.out.println("Have an excellent time next time you go " + activity + ", " + name + "!");
+
+            //This asks the user how much they would like to donate.
         System.out.println("How much would you like to donate today, " + name + "?");
+
+            //This defines the user's next input as a double, accounting for donations with decimals.
         double donation = Double.parseDouble(sc.nextLine());
-        System.out.println("Thanks for donating $" + donation + ". Have a great day!");
+
+            //This thanks the user for donating and wishes them well.
+        System.out.println("Thanks for donating $" + donation + "0. Have a great day, " + name + "!");
+
         }
     }
